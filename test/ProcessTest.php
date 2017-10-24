db05fbe72578e6ae14c33b3d14488250d2d34ed7
peter279k <peter279k@gmail.com>
2017-10-24 19:04:58 +0800

    add more tests

--- test/ProcessTest.php
+++ test/ProcessTest.php
@@ -3,6 +3,8 @@
 namespace Amp\Test\Process;
 
 use Amp\Loop;
+use Amp\ByteStream\ResourceInputStream;
+use Amp\ByteStream\ResourceOutputStream;
 use Amp\Process\Process;
 use PHPUnit\Framework\TestCase;
 
@@ -57,6 +59,186 @@ class ProcessTest extends TestCase {
             $this->assertFalse($completed);
             $this->assertInternalType('int', $process->getPid());
         });
+    }
+
+    public function testCommandArray() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS]);
+            $process->start();
+            $promise = $process->join();
+
+            $completed = false;
+            $promise->onResolve(function () use (&$completed) { $completed = true; });
+            $this->assertFalse($completed);
+            $this->assertInternalType('int', $process->getPid());
+        });
+    }
+
+    public function testProcessCanTerminate() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS]);
+            $process->start();
+            $promise = $process->join();
+
+            $completed = false;
+            $promise->onResolve(function () use (&$completed) { $completed = true; });
+            $process->signal(0);
+            $this->assertInternalType('int', $process->getPid());
+        });
+    }
+
+    public function testGetWorkingDirectoryIsDefault() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS]);
+            $this->assertSame(getcwd(), $process->getWorkingDirectory());
+        });
+    }
+
+    public function testGetWorkingDirectoryIsCustomized() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS], __DIR__);
+            $this->assertSame(__DIR__, $process->getWorkingDirectory());
+        });
+    }
+
+    public function testGetEnv() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS], null, []);
+            $this->assertCount(0, $process->getEnv());
+        });
+    }
+
+    public function testGetStdinIsCustomized() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS], null, [], [
+                new ResourceInputStream(fopen(__DIR__.'/../composer.json', 'r')),
+            ]);
+            $process->start();
+            $promise = $process->join();
+            $completed = false;
+            $promise->onResolve(function () use (&$completed) { $completed = true; });
+            $this->assertInstanceOf(ResourceOutputStream::class, $process->getStdin());
+        });
+    }
+
+    public function testGetStdoutIsCustomized() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS], null, [], [
+                new ResourceOutputStream(fopen(__DIR__.'/../composer.json', 'w')),
+            ]);
+            $process->start();
+            $promise = $process->join();
+            $completed = false;
+            $promise->onResolve(function () use (&$completed) { $completed = true; });
+            $this->assertInstanceOf(ResourceInputStream::class, $process->getStdout());
+        });
+    }
+
+    public function testGetStderrIsCustomized() {
+        Loop::run(function () {
+            $process = new Process([self::CMD_PROCESS], null, [], [
+                new ResourceOutputStream(fopen(__DIR__.'/../composer.json', 'w')),
+            ]);
+            $process->start();
+            $promise = $process->join();
+            $completed = false;
+            $promise->onResolve(function () use (&$completed) { $completed = true; });
+            $this->assertInstanceOf(ResourceInputStream::class, $process->getStderr());
+        });
+    }
+
+    public function testProcessEnvIsValid() {
+        $process = new Process([self::CMD_PROCESS], null, [
+            'env_value'
+        ]);
+        $process->start();
+        $promise = $process->join();
+        $completed = false;
+        $promise->onResolve(function () use (&$completed) { $completed = true; });
+        $this->assertSame('env_value', $process->getEnv()[0]);
+    }
+
+    public function testProcessDeferredIsFailed() {
+        $process = new Process(new \Exception());
+        $this->assertNull($process->start());
+    }
+
+    /**
+     * @expectedException \Error
+     */
+    public function testProcessEnvIsInvalid() {
+        $process = new Process([self::CMD_PROCESS], null, [
+            ['error_value']
+        ]);
+    }
+
+    /**
+     * @expectedException \Amp\Process\StatusError
+     * @expectedExceptionMessage The process has not been started
+     */
+    public function testGetStdinIsNull() {
+        $process = new Process([self::CMD_PROCESS], null, []);
+        $process->getStdin();
+    }
+
+    /**
+     * @expectedException \Amp\Process\StatusError
+     * @expectedExceptionMessage The process has not been started
+     */
+    public function testGetStdoutIsNull() {
+        $process = new Process([self::CMD_PROCESS], null, []);
+        $process->getStdout();
+    }
+
+    /**
+     * @expectedException \Amp\Process\StatusError
+     * @expectedExceptionMessage The process has not been started
+     */
+    public function testGetStderrIsNull() {
+        $process = new Process([self::CMD_PROCESS], null, []);
+        $process->getStderr();
+    }
+
+    /**
+     * @expectedException \Amp\Process\StatusError
+     * @expectedExceptionMessage The process has not been started
+     */
+    public function testProcessCanReset() {
+        $process = new Process(self::CMD_PROCESS);
+        $process->start();
+        $promise = $process->join();
+        $completed = false;
+        $promise->onResolve(function () use (&$completed) { $completed = true; });
+        $processReset = clone $process;
+        $processReset->getPid();
+    }
+
+    /**
+     * @expectedException \Amp\Process\StatusError
+     * @expectedExceptionMessage The process is not running
+     */
+    public function testProcessResetDeferredIsNull() {
+        $process = new Process(self::CMD_PROCESS);
+        $process->start();
+        $promise = $process->join();
+        $completed = false;
+        $promise->onResolve(function () use (&$completed) { $completed = true; });
+        $processReset = clone $process;
+        $processReset->join();
+    }
+
+    /**
+     * @expectedException \Amp\Process\StatusError
+     * @expectedExceptionMessage The process is not running
+     */
+    public function testProcessResetSignalIsNotRunning() {
+        $process = new Process(self::CMD_PROCESS);
+        $process->start();
+        $promise = $process->join();
+        $completed = false;
+        $promise->onResolve(function () use (&$completed) { $completed = true; });
+        $processReset = clone $process;
+        $processReset->signal(1);
     }
 
     /**
