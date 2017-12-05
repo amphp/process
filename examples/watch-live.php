9d271e1be37fed874718b3d11c07b1653184d09f
Niklas Keller <me@kelunik.com>
2017-12-05 09:15:04 +0100

    Make examples/watch-live.php work on Windows

--- examples/watch-live.php
+++ examples/watch-live.php
@@ -5,7 +5,11 @@ include dirname(__DIR__) . "/vendor/autoload.php";
 use Amp\Process\Process;
 
 Amp\Loop::run(function () {
-    $process = new Process("echo 1; sleep 1; echo 2; sleep 1; echo 3; exit 42");
+    // abuse ping for sleep, see https://stackoverflow.com/a/1672349/2373138
+    $command = \DIRECTORY_SEPARATOR === "\\"
+        ? "cmd /c echo 1 & ping -n 2 127.0.0.1 > nul & echo 2 & ping -n 2 127.0.0.1 > nul & echo 3 & exit 42"
+        : "echo 1; sleep 1; echo 2; sleep 1; echo 3; exit 42";
+    $process = new Process($command);
     $process->start();
 
     $stream = $process->getStdout();
