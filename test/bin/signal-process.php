e9f1681fab0f94252f2956bfda5f01596932ac04
Aaron Piotrowski <aaron@trowski.com>
2019-02-26 10:22:25 -0600

    Missed adding signal test script
    Whoops…

---
+++ test/bin/signal-process.php
@@ -0,0 +1,13 @@
+<?php
+
+require dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+Amp\Loop::run(function () {
+    Amp\Loop::unreference(Amp\Loop::onSignal(\SIGTERM, function () {
+        exit(42);
+    }));
+
+    Amp\Loop::delay(1000, function () {
+        exit(0);
+    });
+});
