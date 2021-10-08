9b62be73a4c68e4005823b54a9fd021f2888b9e3
Konstantin Grachev <me@grachevko.ru>
2021-10-08 17:55:53 +0200

    Prevent function redeclare while using preload (#53)
    (cherry picked from commit 3d36327bf9b4c158cf3010f8f65c00854ec3a8b7)

--- src/functions.php
+++ src/functions.php
@@ -5,30 +5,32 @@ namespace Amp\Process;
 const BIN_DIR = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'bin';
 const IS_WINDOWS = \PHP_OS_FAMILY === 'Windows';
 
-if (IS_WINDOWS) {
-    /**
-     * Escapes the command argument for safe inclusion into a Windows command string.
-     *
-     * @param string $arg
-     *
-     * @return string
-     */
-    function escapeArgument(string $arg): string
-    {
-        return '"' . \preg_replace_callback('(\\\\*("|$))', function (array $m): string {
-            return \str_repeat('\\', \strlen($m[0])) . $m[0];
-        }, $arg) . '"';
-    }
-} else {
-    /**
-     * Escapes the command argument for safe inclusion into a Posix shell command string.
-     *
-     * @param string $arg
-     *
-     * @return string
-     */
-    function escapeArgument(string $arg): string
-    {
-        return \escapeshellarg($arg);
+if (!\function_exists(__NAMESPACE__ . '\\escapeArgument')) {
+    if (IS_WINDOWS) {
+        /**
+         * Escapes the command argument for safe inclusion into a Windows command string.
+         *
+         * @param string $arg
+         *
+         * @return string
+         */
+        function escapeArgument(string $arg): string
+        {
+            return '"' . \preg_replace_callback('(\\\\*("|$))', function (array $m): string {
+                return \str_repeat('\\', \strlen($m[0])) . $m[0];
+            }, $arg) . '"';
+        }
+    } else {
+        /**
+         * Escapes the command argument for safe inclusion into a Posix shell command string.
+         *
+         * @param string $arg
+         *
+         * @return string
+         */
+        function escapeArgument(string $arg): string
+        {
+            return \escapeshellarg($arg);
+        }
     }
 }
